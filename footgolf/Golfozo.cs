99927110773a5ae5af969621fe55aa9b20ddf73a
szalokiszotyi <55582383+szalokiszotyi@users.noreply.github.com>
2020-03-26 11:57:53 +0100

    Add project files.

---
+++ footgolf/Golfozo.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace footgolf
+{
+    class Golfozo
+    {
+        public string Nev { get; set; }
+        public string Kategoria { get; set; }
+        public string Egyesület { get; set; }
+        public byte[] Pontok { get; set; }
+
+
+        public Golfozo(string sor)
+        {
+            string[] m = sor.Split(';');
+
+            Nev = m[0];
+            Kategoria = m[1];
+            Egyesület = m[2];
+            Pontok = new byte[8];
+            for (int i = 0; i < Pontok.Length; i++)
+            {
+                Pontok[i] = byte.Parse(m[i + 3]);
+            }
+
+        }
+
+        public int osszpontszam
+        {
+            get
+            {
+                int osszpont = 0;
+                Array.Sort(Pontok);
+                for (int i = 2; i < Pontok.Length; i++)
+                {
+                    osszpont += Pontok[i];
+                }
+                if (Pontok[0] != 0) osszpont += 10;
+                if (Pontok[1] != 0) osszpont += 10;
+                return osszpont;
+            }
+        }
+
+    }
+}
---
+++ footgolf/Program.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace footgolf
+{
+    class Program
+    {
+        static List<Golfozo> versenyzok;
+        static void Main(string[] args)
+        {
+            Beolvas();
+            F03();
+            F04();
+            F06();
+            F07();
+            F08();
+            Console.ReadKey();
+        }
+
+        private static void F08()
+        {
+            
+            Console.WriteLine("f08:");
+        }
+
+        private static void F07()
+        {
+                var sw = new StreamWriter(
+                    "osszpontff.txt", false, Encoding.UTF8);
+                versenyzok.ForEach(
+                    a => sw.WriteLine($"f07: {a.Nev}({a.Kategoria == "Felnott ferfi"});{a.osszpontszam}"));
+                sw.Close();
+        }
+
+        private static void F06()
+        {
+
+                Golfozo r = versenyzok
+                    .Where(f => f.Kategoria == "Noi")
+                    .OrderByDescending(f => f.osszpontszam)
+                    .FirstOrDefault();
+
+                Console.WriteLine($"f06: A bajnok női versenyző \n Név: {r.Nev}\n Egyesület: {r.Egyesület} \n Összespont: {r.osszpontszam}");
+        }
+
+        private static void F04()
+        {
+            decimal osszferfi = versenyzok.Count(f => f.Kategoria=="Felnott ferfi");
+            decimal ossznoi = versenyzok.Count(f => f.Kategoria=="Noi");
+            
+            Console.WriteLine("f04: " + (ossznoi/osszferfi)*100 + "% a női tagok aránya");
+        }
+
+        private static void F03()
+        {
+            Console.WriteLine($"f03: {versenyzok.Count}");
+        }
+
+        private static void Beolvas()
+        {
+            versenyzok = new List<Golfozo>();
+            var sr = new StreamReader("fob2016.txt", Encoding.UTF8);
+
+            while (!sr.EndOfStream)
+            {
+                versenyzok.Add(new Golfozo(sr.ReadLine()));
+            }
+            sr.Close();
+        }
+    }
+}
